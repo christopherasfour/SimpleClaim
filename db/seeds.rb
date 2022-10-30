2890567816a0d78b9278d834a15d28b0b2767668
christopherasfour <52343098+christopherasfour@users.noreply.github.com>
2022-10-30 11:00:19 -0700

    fix claimType to claimTypes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 claims = [
-  { fname: 'Christopher', lname: 'Asfour', bday: '21-Jul-1989', claimType: 'Breach of contract disputes', description: 'Testing testing' },
-  { fname: 'Luke', lname: 'Cage', bday: '21-Jul-1990', claimType: 'Pick', description: 'Testing 123' },
-  { fname: 'Richard', lname: 'Mike', bday: '21-Aug-1989', claimType: 'Pick', description: 'Testing 1111' },
+  { fname: 'Christopher', lname: 'Asfour', bday: '21-Jul-1989', claimTypes: 'Breach of contract disputes', description: 'Testing testing' },
+  { fname: 'Luke', lname: 'Cage', bday: '21-Jul-1990', claimTypes: 'Pick', description: 'Testing 123' },
+  { fname: 'Richard', lname: 'Mike', bday: '21-Aug-1989', claimTypes: 'Pick', description: 'Testing 1111' },
 
 ]
 
 claims.each do |claim|
   Claim.create!(claim)
 end
+
