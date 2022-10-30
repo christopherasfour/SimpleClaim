e4ea2169fd7c379d1e88326f506c115aa409f0cb
christopherasfour <52343098+christopherasfour@users.noreply.github.com>
2022-10-30 15:10:08 -0700

    test in model

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -1,5 +1,5 @@
 class Claim < ActiveRecord::Base
-    belongs_to :lawyer, foreign_key: 'requestLawyer'
+    belongs_to :lawyer, foreign_key: 'lawyer_id'
     def self.types
         [
             "Pick",
