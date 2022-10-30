5672c435bebc788dd481aa4d56eec60941d430d8
dh9090 <103529631+dh9090@users.noreply.github.com>
2022-10-30 08:11:04 -0700

    Update routes.rb
    Change routes for index and claim

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   resources :users, only: [:new, :create]
+  get 'home', :to => "welcome#index"
+  root :to => "welcome#index"
+  get 'claim/new', to: 'sessions#claim'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
@@ -9,6 +14,6 @@ Rails.application.routes.draw do
 
   resources :claim
   # map '/' to be a redirect to '/claim'
-  root :to => redirect('/claim')
+  #root :to => redirect('/claim')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
