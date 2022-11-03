2366438bada91ab039886f45d7b78588fbf4b52f
Sameer Khanna <stk2137@columbia.edu>
2022-11-03 03:46:55 +0000

    Add rspec test

--- spec/controllers/claim_controller_spec.rb
+++ spec/controllers/claim_controller_spec.rb
@@ -1,40 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe ClaimController do
-    describe "POST #create" do
-      it "should create a new claim" do
-        post :create, FactoryGirl.attributes_for(:claim)
-        post :create, {claim: {fname: "Joey", lname: "Asfour", bday:"09-04-2001", claimTypes: "Pick", description: "Hello there everyone!", lawyers_id: 10 }}
-        expect(Claim.find_by(fname: 'Joey')).should_not be_nil
-        # expect {post :create, claim: FactoryGirl.attributes_for(:claim)
-        # }.to change { Claim.count }.by(1)
-      end
-    end
-
     describe "POST create" do
+      before do
+        visit "/register"
+        fill_in "user_username", :with => "test_user"
+        fill_in "user_password", :with => "test_pass"
+        click_button "Create User"
+        expect(page).to have_content "Welcome"
+      end
+      
       context "with valid attributes" do
-        it "creates a new contact" do
-          expect{
-            post :create, claim: FactoryGirl.attributes_for(:claim)
-          }.to change(Claim,:count).by(1)
+
+        it "is valid with valid attributes" do
+          expect(Claim.new(fname: "Asfour", lname: "Asfour", bday:"09-04-2001", claimTypes: "Pick", description: "Hello there everyone!", lawyers_id: 10)).to be_valid
         end
-    
-        it "redirects to the new contact" do
-          post :create, claim: FactoryGirl.attributes_for(:claim)
-          response.should redirect_to Claim.last
-        end
+
       end
     
       context "with invalid attributes" do
+
         it "does not save the new contact" do
-          expect{
-            post :create, claim: FactoryGirl.attributes_for(:invalid_claim)
-          }.to_not change(Contact,:claim)
-        end
-    
-        it "re-renders the new method" do
-          post :create, claim: FactoryGirl.attributes_for(:invalid_claim)
-          response.should render_template :new
+          expect { 
+            post :create, {claim: { fname: 'Christopher', lname: 'Asfour', bday: '21-Jul-1989', claimTypes: 'Breach of contract disputes', description: 'Testing testing', lawyers_id: 1 }}
+          }.to change(Claim, :count).by(0)
         end
       end 
     end
