008ac11878d9fdf83cae6e69dc95fda48ca31a0d
dh9090 <103529631+dh9090@users.noreply.github.com>
2022-11-14 15:47:26 -0800

    Add more test cases for user

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -11,4 +11,31 @@ RSpec.describe UsersController, :type => :controller do
       end
     end
   end
+  describe "POST create" do
+    context "with valid attributes" do
+      it "is invalid with just password field" do
+        expect {post :create, {user: {username: "", password: "justpassword"}}
+        }.to change { User.count }.by(0)
+        assert_redirected_to "/register"
+      end
+    end
+  end
+  describe "POST create" do
+    context "with valid attributes" do
+      it "is invalid with just username field" do
+        expect {post :create, {user: {username: "justusername", password: ""}}
+        }.to change { User.count }.by(0)
+        assert_redirected_to "/register"
+      end
+    end
+  end
+  describe "POST create" do
+    context "with valid attributes" do
+      it "is valid with both fields" do
+        expect {post :create, {user: {username: "validusername", password: "validpassword"}}
+      }.to change { User.count }.by(1)
+        assert_redirected_to "/welcome"
+      end
+    end
+  end
 end
